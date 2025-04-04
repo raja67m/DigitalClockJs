d8ec931592884b8382e8e6dddb6711cb5dfd8233
Raja <rajalingam210901@gmail.com>
2025-04-04 18:58:35 +0530

    Javascript function completed

--- clock.js
+++ clock.js
@@ -1,10 +1,10 @@
-var h=document.querySelector('.h');
-var m=document.querySelector('.s');
-var s=document.querySelector('.m');
-var days=document.querySelector('.daysspan');
+var h=document.querySelectorAll('.h');
+var m=document.querySelectorAll('.s');
+var s=document.querySelectorAll('.m');
+var days=document.querySelectorAll('.days span');
 var date=document.querySelector('.date');
-var month=document.querySelector('.month');
-var span=document.querySelector('.meridian');
+var month=document.querySelectorAll('.month span');
+var span=document.querySelectorAll('.meridian span');
 
 function runClock(){
 var time=new Date();
@@ -16,12 +16,52 @@ da=da<10?'0'+da:da;
 var mo=time.getMonth()+1;
 mo=mo<10? '0'+mo:mo;
 var ye=time.getFullYear();
-console.log(da,mo,ye);
+
 //date
 date.innerHTML=`${da}-${mo}-${ye}`;
-console.log(time.getDay());
+
 //day
-days[time.getDay()].classList.add('day-active');
+days[time.getDay()].classList.add('days-active');
+// month
+month[time.getMonth()].classList.add('month-active');
+
+// time function
+var hrs=time.getHours();
+var min=time.getMinutes();
+var sec=time.getSeconds();
+
+if(hrs>12){
+   hrs=hrs-12;
+   span[1].classList.add('active');
+  
+}
+else{
+   if(hrs==0){
+      hrs=12;
+   }
+   span[0].classList.add('active');
+ 
 
 }
-runClock();
+
+
+hrs=hrs<10?'0'+hrs:hrs;
+min=min<10?'0'+min:min;
+sec=sec<10?'0'+sec:sec;
+
+hrs=hrs.toString();
+min=min.toString();
+sec=sec.toString();
+
+h[0].innerHTML=hrs[0];
+h[1].innerHTML=hrs[1];
+
+m[0].innerHTML=min[0];
+m[1].innerHTML=min[1];
+
+s[0].innerHTML=sec[0];
+s[1].innerHTML=sec[1];
+
+
+}
+setInterval(runClock,1000);
